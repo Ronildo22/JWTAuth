207dd41e7c67f168e1b25b4540e4abfac712ad91
Ronildo22 <ronildo.santos224@gmail.com>
2025-07-28 01:05:54 -0300

    feat(error-handler): add rate limit error handler and integrate flask-limiter

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 
 from src.auth.route import bp_auth
+from src.error_handler.error_handler import bp_erro_handler
 
 
 def create_app():
@@ -14,4 +18,13 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
+    app.register_blueprint(bp_erro_handler)
+
+    # Rate Limiter Configuration
+    limiter = Limiter(
+        get_remote_address,
+        app=app,
+        default_limits=["100 per minute"]
+    )
+
     app.run(host="0.0.0.0", port=3000, debug=True)
